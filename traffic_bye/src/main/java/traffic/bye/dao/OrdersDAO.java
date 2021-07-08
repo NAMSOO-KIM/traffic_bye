e538b60351ddc6ccea451de54efa7cba897d9ffc
EuJin <qksdjwls@gmail.com>
2021-07-08 09:59:07 +0900

    back up/오더 알림 + 오더 목록

--- traffic_bye/src/main/java/traffic/bye/dao/OrdersDAO.java
+++ traffic_bye/src/main/java/traffic/bye/dao/OrdersDAO.java
@@ -1,8 +1,10 @@
 package traffic.bye.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import traffic.bye.vo.OrdersDetailVO;
+import traffic.bye.vo.OrdersManageVO;
 import traffic.bye.vo.OrdersVO;
 
 public interface OrdersDAO {
@@ -10,5 +12,9 @@ public interface OrdersDAO {
 	int insertOrders(OrdersVO ordersVO) throws SQLException;
 	
 	int insertOrdersDetail(OrdersDetailVO ordersDetailVO) throws SQLException;
+
+	List<OrdersManageVO> getOrderList(long storeId) throws SQLException;
+	
+	long getOrderId() throws SQLException; 
 	
 }
