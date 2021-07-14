c82d57ec579f607ba6b442944992a96f8e88a072
bobo <rlaxogud94@naver.com>
2021-07-14 22:02:39 +0900

    관리자 계정 테스트

--- traffic_bye/src/test/java/traffic/bye/service/MemberServiceTest.java
+++ traffic_bye/src/test/java/traffic/bye/service/MemberServiceTest.java
@@ -23,5 +23,20 @@ public class MemberServiceTest {
 	public void test() throws Exception{
 	//	MemberVO memberVO = new MemberVO(null, null, "test3", "1", "1234567", "1234", "남수3", "1234", ROLE.ADMIN, AuthType.PHONE);
 		//memberService.memberRegister(memberVO);
+		//MemberVO memberVO  = new MemberVO(null, null , "admin1", "1", "1234", "1234", "삼성전자", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1234");
+		MemberVO memberVO1  = new MemberVO(null, null , "admin1", "1", "1234", "1234", "삼성전자", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1235");
+		MemberVO memberVO2  = new MemberVO(null, null , "admin2", "2", "1234", "1234", "캐논", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1236");
+		MemberVO memberVO3  = new MemberVO(null, null , "admin3", "3", "1234", "1234", "소니", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1237");
+		MemberVO memberVO4  = new MemberVO(null, null , "admin4", "4", "1234", "1234", "담배매장", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1238");
+		MemberVO memberVO5  = new MemberVO(null, null , "admin5", "5", "1234", "1234", "주류매장", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1239");
+		MemberVO memberVO6  = new MemberVO(null, null , "admin6", "6", "1234", "1234", "정관장", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1240");
+		MemberVO memberVO7  = new MemberVO(null, null , "admin7", "7", "1234", "1234", "뱅앤올룹슨", "2016-09-26", ROLE.ADMIN, AuthType.PHONE, "1241");
+		memberService.memberRegister(memberVO1);
+		memberService.memberRegister(memberVO2);
+		memberService.memberRegister(memberVO3);
+		memberService.memberRegister(memberVO4);
+		memberService.memberRegister(memberVO5);
+		memberService.memberRegister(memberVO6);
+		memberService.memberRegister(memberVO7);
 	}
 }
